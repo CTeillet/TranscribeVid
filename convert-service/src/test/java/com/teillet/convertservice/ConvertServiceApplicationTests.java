e070c0cddc357778dc3336e77980fb1235449579
Teillet Corentin <teillet.corentin@neuf.fr>
2024-12-28 23:55:10 +0100

    autres fichiers

---
+++ convert-service/src/test/java/com/teillet/convertservice/ConvertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teillet.convertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ correction-service/src/test/java/com/teillet/correctionservice/CorrectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teillet.correctionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
