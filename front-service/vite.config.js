5de4427b20d1aa9e84ae4fc0e89f08f6b97fee07
Teillet Corentin <teillet.corentin@neuf.fr>
2025-01-04 15:32:01 +0100

    3 : Création DockerFile pour front-service

--- front-service/vite.config.js
+++ front-service/vite.config.js
@@ -1,16 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import compress from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [
+        vue(),
+        compress({
+            algorithm: 'gzip', // Ou 'brotliCompress' pour Brotli
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        target: 'esnext', // Support moderne pour les navigateurs récents
+        minify: 'terser', // Utilise Terser pour un minification avancée
+        sourcemap: false, // Désactive les sourcemaps pour réduire la taille
+        rollupOptions: {
+            output: {
+                manualChunks: undefined, // Tout regrouper si nécessaire
+            },
+        },
+    },
 })
